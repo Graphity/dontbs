3cde3992074ab1743f46704c23de04089ae1d100
Graphity <rohood3@gmail.com>
2022-07-13 23:12:44 +0400

    Test scraper

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
+from scraper import GHScraper
 
-username = 'Graphity'
-
-url = f'https://github.com/users/{username}/contributions'
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
-
-h2 = soup.find('h2').text
-h2 = ' '.join(h2.split())
-print(h2)
-
-
-year = []
-
-table = soup.find('svg').find('g')
-for column in table.find_all('g'):
-    week = []
-    for cell in column.find_all('rect'):
-        week.append(
-            {
-                'count': cell['data-count'],
-                'date': cell['data-date'],
-                'level': cell['data-level']
-            }
-        )
-    year.append(week)
-
+s = GHScraper('Graphity')
+year = s.fetch_contributions()
 
 for week in year:
     for day in week:
@@ -34,3 +9,5 @@ for week in year:
         print(day['date'])
         print(day['level'])
         print()
+
+print(s.contributions)
